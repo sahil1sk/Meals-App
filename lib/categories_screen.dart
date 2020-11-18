0f8e48911a67e30339e47f97e105108e7d712327
sahil1sk <khannasahil303@gmail.com>
2020-11-18 15:26:24 +0530

    Register Screen as Main Screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -6,21 +6,25 @@ import './category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-        .map((catData) => CategoryItem(
-            catData.title,
-            catData.color,
-          )
-        ).toList(),
-      // Like listView we have GridView, we can also make GridViewBuilder For dynamic like we do for ListView Builder
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,  // width of each box
-        childAspectRatio: 3 / 2,  // height
-        crossAxisSpacing: 20,  // generating the spacing
-        mainAxisSpacing: 20,   // for spacing
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'), 
       ),
-      
-    );
+      body: GridView(
+        children: DUMMY_CATEGORIES
+          .map((catData) => CategoryItem(
+              catData.title,
+              catData.color,
+            )
+          ).toList(),
+        // Like listView we have GridView, we can also make GridViewBuilder For dynamic like we do for ListView Builder
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,  // width of each box
+          childAspectRatio: 3 / 2,  // height
+          crossAxisSpacing: 20,  // generating the spacing
+          mainAxisSpacing: 20,   // for spacing
+        ),  
+      )
+    ); 
   }
 }
