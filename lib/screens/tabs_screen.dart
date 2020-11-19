a21ac6b07511bbbffd1a088942d08d2344337c20
sahil1sk <khannasahil303@gmail.com>
2020-11-19 17:28:05 +0530

    Adding Bottom TabBar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,34 +8,55 @@ class TabsScreen extends StatefulWidget {
   _TabsScreenState createState() => _TabsScreenState();
 }
 
+// Note:- the only difference b/w appBar TabBar and Bottom TabBar is that
+// the appBar tabBar is controlled auto by flutter but bottom tabBar we will control that
+
 class _TabsScreenState extends State<TabsScreen> {
+  // making final list of pages
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(), 
+      'title': 'Categories'
+    },
+    {
+      'page': FavoritesScreen(), 
+      'title': 'Your Favorite'
+    },
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index){ // auto get index by flutter which tab we select
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,  // how many tabs we want
-      initialIndex: 1, // which tab we want to show first // 0 is the default
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar( // setting all items inside the tab
-            tabs: <Widget>[ // we add 2 Tab in list because we choose length of 2
-              Tab(
-                icon: Icon(Icons.category), // adding icon to the tab
-                text: 'Category', // adding text on the tab
-              ), 
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'], // setting the page which we want to show
+      bottomNavigationBar: BottomNavigationBar( // adding so that our tab will show at bottom
+        onTap: _selectPage, // in this we pass the index on which item we click so set according to there index
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex, // so here we pass which item is selected now
+        type: BottomNavigationBarType.shifting, // for giving animation effect
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            label: 'Categories',
           ),
-        ),
-        body: TabBarView( // setting tabBar View
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
       ),
     );
   }
